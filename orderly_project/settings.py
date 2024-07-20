d67ff6be444a4d11eb1ad36ead980499b825d8ac
tedi005 <tedidervishi@gmail.com>
2024-07-20 13:28:31 +0200

    auth2

--- orderly_project/settings.py
+++ orderly_project/settings.py
@@ -125,13 +125,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets'
+]
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 
-MEDIA_URL="/media/"
-MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
+MEDIA_URL="media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
