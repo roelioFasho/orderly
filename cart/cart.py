26a5df88addb48eda1aa0aa05ef8e35da9fbd8d0
tedi005 <tedidervishi@gmail.com>
2024-08-03 03:48:29 +0200

    cart

---
+++ cart/cart.py
@@ -0,0 +1,67 @@
+from base.models import Product
+
+class Cart():
+    def __init__(self, request):
+        self.session = request.session
+
+        #get request
+        self.request = request
+        # get the current session key if it exists
+        cart = self.session.get('session_key')
+
+        # if thr user is new, no session key. If not create one
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+
+        # makes url the cart is available on all pages of the site
+        self.cart = cart
+
+
+    
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        quantity_qty = str(quantity) 
+
+
+        #logic
+        if product_id in self.cart:
+            pass
+        else:
+            # self.cart[product_id] = {'price':  str(product.price)}
+            self.cart[product_id] = int(quantity_qty)
+
+
+        self.session.modified = True
+
+
+    def __len__(self):
+        return(len(self.cart))
+    
+
+    def get_products(self):
+        #get ids
+        product_ids = self.cart.keys()   
+        #look at the db
+        products = Product.objects.filter(id__in=product_ids)
+    
+        return products
+    
+
+    def get_quantities(self):
+        #get ids
+        quantities = self.cart  
+        return quantities
+    
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        #get cart
+        ourcart = self.cart
+        #update cart/dict
+        ourcart[product_id] = product_qty
+
+        self.session.modified = True
+        think = self.cart
+        return think
